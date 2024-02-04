872830b9967185c5e35db8eeca844c13f3682aad
Hritwick <51262404+Hritwick14@users.noreply.github.com>
2024-02-04 17:40:51 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,10 +1,34 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(windowWidth, windowHeight);
 }
+
 function draw() {
-  circle(mouseX,mouseY,50)
-  // background(220);
+  let x = windowWidth/2
+  let y = windowHeight/2
+  let radius = 30
+  
+  beginShape()
+  vertex(x,y-radius)
+  vertex(x-(radius*cos(60)),y+(radius*sin(60)))
+  vertex(x-(radius*cos(60)),y-(radius*sin(60)))
+  vertex(x,y+radius)
+  vertex(x+(radius*cos(60)),y-(radius*sin(60)))
+  vertex(x+(radius*cos(60)),y+(radius*sin(60)))
+  endShape(CLOSE)
+  
+  fill("green")
+  circle(x,y, 10)
+  noFill()
+  
+  fill("red")
+  circle(x,y-radius, 10)
+  noFill()
+  
+  // console.log(3,4,5)
+  
+  circle(x+(radius*cos(60)),y+((radius*sin(60))), 10)
 }
+
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
 }
